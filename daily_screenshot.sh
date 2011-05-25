9c44af9419a64bd14e97d00f4d2c6daccf1594ba
muflax <mail@muflax.com>
2011-05-25 11:21:06 +0200

    bugfix

--- daily_screenshot.sh
+++ daily_screenshot.sh
@@ -3,4 +3,6 @@
 format="%Y-%m-%d_%H-%M_$(hostname).jpg" 
 exec="mv \$f $HOME/pigs/daily/"
 
-[[ $(pidof X) > 0 ]] && DISPLAY=:0.0 scrot -m $format -q 30 -e "$exec"
+if [[ $(pidof X) -gt 0 ]]; then
+  DISPLAY=:0.0 scrot -m $format -q 30 -e "$exec"
+fi
