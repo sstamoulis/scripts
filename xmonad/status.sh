60645310a0635803bd3f8b7132016077371b954d
muflax <mail@muflax.com>
2011-05-26 12:12:51 +0200

    also update timetrap every 10 minutes

--- xmonad/status.sh
+++ xmonad/status.sh
@@ -62,9 +62,13 @@ status() {
 # watches timetrap database for changes to prevent unnecessary execs
 watch_timetrap() {
   mod_time=$(stat -c "%Y" $timetrap_db)
+  now=$(date "+%s")
   
   if [[ $mod_time -gt $last_mod_time ]]; then
     last_mod_time=$mod_time
+    ti display all --start 'today 0:00' -f status
+  elif [[ $now -gt $(( $last_mod_time + 600 )) ]]; then
+    last_mod_time=$now
     ti display all --start 'today 0:00' -f status
   fi
 }
