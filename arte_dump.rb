47442e3cb86a4787e78d1067dd778be509940005
muflax <mail@muflax.com>
2011-07-22 04:02:06 +0200

    fixed arte script

--- arte_dump.rb
+++ arte_dump.rb
@@ -8,39 +8,41 @@ require "open-uri"
 lang = "de"
 
 ARGV.each do |url| 
-    arte_path = "/home/amon/映画/arte"
-    target = "#{arte_path}/#{File::basename(url).sub(".html", ".flv")}"
+  arte_path = "/home/amon/映画/arte"
+  Dir.mkdir(arte_path)
+  target = "#{arte_path}/#{File::basename(url).sub(".html", ".flv")}"
 
-    site = open(url)
+  site = open(url)
 
-    # find the swf player and next xml file
-    player = rtmp = site2 = site3 = ""
-    site.each do |line| 
-        if m = line.match(%r|http://videos\.arte\.tv.*?\.swf|)
-            player = m[0]
-        elsif m = line.match(%r|http://.*?\,view\,asPlayerXml\.xml|)
-            site2 = open(m[0])
-        end
+  # find the swf player and next xml file
+  player = rtmp = site2 = site3 = ""
+  site.each do |line| 
+    if m = line.match(%r|http://videos\.arte\.tv.*?\.swf|)
+      player = m[0]
+    elsif m = line.match(%r|http://.*?\,view\,asPlayerXml\.xml|)
+      site2 = open(m[0])
     end
+  end
 
-    # find final xml file
-    site2.each do |line| 
-        if m = line.match(%r|http://videos\.arte\.tv/#{lang}.*?\,view\,asPlayerXml\.xml|)
-            site3 = open(m[0])                     
-            break
-        end
+
+  # find final xml file
+  site2.each do |line| 
+    if m = line.match(%r|http://videos\.arte\.tv/#{lang}.*?\,view\,asPlayerXml\.xml|)
+      site3 = open(m[0])                     
+      break
     end
+  end
 
-    # find rtmp address
-    site3.each do |line| 
-        if line.match(/quality="hd"/)
-            rtmp = line[/rtmp:.*(?=<\/url>)/]
-            break
-        end
+  # find rtmp address
+  site3.each do |line| 
+    if line.match(/quality="hd"/)
+      rtmp = line[/rtmp:.*(?=<\/url>)/]
+      break
     end
+  end
 
-    # finally download
-    system("rtmpdump --rtmp '#{rtmp}' --swfVfy '#{player}' --flv '#{target}'")
+  # finally download
+  system("rtmpdump --rtmp '#{rtmp}' --swfVfy '#{player}' --flv '#{target}'")
 end
 
 # delete .swfinfo
