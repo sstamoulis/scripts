d2e8598cbeb4c14def127beb7227e37c0c21a516
muflax <mail@muflax.com>
2011-10-28 20:52:44 +0200

    simplified s3 backup

--- s3-backup.sh
+++ s3-backup.sh
@@ -4,14 +4,7 @@
 
 S3=s3://backup.muflax.com
 
-echo "Cloning ~/spoiler..."
-git clone ~/spoiler ~/spoiler-backup
-
 echo "Backuping up to $S3..."
-s3cmd sync --delete-removed ~/spoiler-backup/ $S3/spoiler/
-s3cmd sync --delete-removed ~/spoiler/mail/ $S3/mail/
-
-echo "Deleting temp files..."
-rm -vrf ~/spoiler-backup
+s3cmd sync --delete-removed ~/spoiler/ $S3/spoiler/
 
 echo "Backup done."
