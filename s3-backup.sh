f94cc62e531dacdb48f4cbd58ca062f1d1e35ea8
muflax <mail@muflax.com>
2011-10-29 11:39:45 +0200

    compress before uploading to minimize cost

--- s3-backup.sh
+++ s3-backup.sh
@@ -2,9 +2,15 @@
 # Copyright muflax <mail@muflax.com>, 2011
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-S3=s3://backup.muflax.com
+S3=s3://backup.muflax.com     
+
+echo "Compressing ~/spoiler..."
+tar -vczf ~/spoiler.tar.gz ~/spoiler             
 
 echo "Backuping up to $S3..."
-s3cmd sync --delete-removed ~/spoiler/ $S3/spoiler/
+s3cmd sync --delete-removed ~/spoiler.tar.gz $S3/
+
+echo "Cleaning up..."
+rm ~/spoiler.tar.gz
 
 echo "Backup done."
