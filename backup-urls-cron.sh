6a8d142f21fac4de882a63436226618c406d74bc
muflax <mail@muflax.com>
2011-11-21 01:09:36 +0100

    bugfix

--- backup-urls-cron.sh
+++ backup-urls-cron.sh
@@ -7,5 +7,6 @@ export PATH="$($HOME/in/scripts/extend_path.sh):$PATH"
 export GEM_HOME="$HOME/local/gems"
 export GEM_PATH="$GEM_HOME"
 export PATH="$GEM_HOME/bin:$PATH"
+export LANG=ja_JP.UTF-8
 
 firefox-urls | backup-urls >/dev/null
