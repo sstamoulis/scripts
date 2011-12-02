085da880f98e90038a47c0066308f9fc4cda16d9
muflax <mail@muflax.com>
2011-12-02 04:45:48 +0100

    removed debug statement

--- s3-backup.sh
+++ s3-backup.sh
@@ -12,7 +12,6 @@ split --line-bytes=100M spoiler.tar.gz spoiler.tar.gz_
 rm spoiler.tar.gz
 
 echo "Backuping up to $S3..."
-read
 s3cmd del -r $S3/spoiler-backup
 s3cmd sync --delete-removed ~/spoiler-backup $S3/
 
