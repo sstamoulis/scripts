944c68580057c80968feb4b5dfcbbc86ebc9564b
muflax <mail@muflax.com>
2011-12-02 04:24:10 +0100

    more universal

--- remember_song.sh
+++ remember_song.sh
@@ -2,4 +2,4 @@
 # Copyright muflax <mail@muflax.com>, 2011
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-mpc current >> $HOME/remembered_songs
+MPD_HOST=192.168.1.15 mpc current >> $HOME/remembered_songs
