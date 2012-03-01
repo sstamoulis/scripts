4c8398cdc35de974d3fa5868cc9b445195757c15
muflax <mail@muflax.com>
2012-03-01 01:04:24 +0100

    encrypt backups

--- s3-backup.sh
+++ s3-backup.sh
@@ -7,9 +7,16 @@ S3=s3://backup.muflax.com
 echo "Compressing ~/spoiler..."
 mkdir -p $HOME/spoiler-backup
 cd $HOME/spoiler-backup
+
+# compress
 tar -vczf spoiler.tar.gz ~/spoiler --exclude ~/spoiler/archive/www
-split --line-bytes=100M spoiler.tar.gz spoiler.tar.gz_
+
+# encrypt
+gpg -r muflax -cev --batch --passphrase-file ~/.s3_passphrase spoiler.tar.gz
 rm spoiler.tar.gz
+
+# split for easier transfer
+split --line-bytes=100M spoiler.tar.gz.gpg spoiler.tar.gz.gpg_
 
 echo "Backuping up to $S3..."
 s3cmd del -r $S3/spoiler-backup
