a87c0627dc1832076916824b2097debce154eed2
muflax <mail@muflax.com>
2013-05-10 17:11:04 +0200

    remove magnet

--- magnet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-# Copyright muflax <mail@muflax.com>, 2012
-# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
-
-# open magnet links
-DISPLAY=:0.0 /usr/bin/transmission-qt "$1" &!
