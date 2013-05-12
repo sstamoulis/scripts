ad2d86aee5bf38a8700d4f1fedb09275613bc26f
muflax <mail@muflax.com>
2013-05-12 21:08:05 +0200

    added another repo

--- git_annex_watchers.sh
+++ git_annex_watchers.sh
@@ -2,7 +2,7 @@
 # Copyright muflax <mail@muflax.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-dirs=(~/books ~/音/ ~/テレビ/)
+dirs=(~/books ~/音/ ~/テレビ/ ~/games/install)
 
 for dir in $dirs; do
   cd $dir
