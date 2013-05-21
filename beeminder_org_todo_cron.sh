8f6bec2b929bb566be24263470e1a8ec1998a4c5
muflax <mail@muflax.com>
2013-05-21 08:45:12 +0200

    beeminder org todo cronjob script

---
+++ beeminder_org_todo_cron.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2012
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+source ~/.zsh/env.sh
+source ~/.zsh/path.sh
+
+beeminder_org_todo.rb -a >/dev/null
