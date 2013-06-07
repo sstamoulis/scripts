3afe9b3d25de8346eb1b55d66e16201f8e851228
muflax <mail@muflax.com>
2013-06-07 06:22:24 +0200

    update both weight goals

---
+++ beeminder/weight.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+# Copyright muflax <mail@muflax.com>, 2013
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+require "beeminder"
+
+if ARGV.empty?
+  puts "usage: weight.rb 80.5"
+  exit 1
+end
+
+weight = ARGV.shift.to_f 
+
+puts "connecting to beeminder..."
+config = YAML.load File.open("#{Dir.home}/.beeminderrc")
+bee    = Beeminder::User.new config["token"]
+
+puts "sending weight..."
+bee.send "weight",   weight
+
+puts "updating weighhing..."
+bee.send "weighing", 1
