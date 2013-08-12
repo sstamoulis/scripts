e590e04937f08edc0b7ae958b62b938951cd5c7d
muflax <mail@muflax.com>
2013-08-12 18:11:35 +0200

    more cron stuff

--- beeminder/beeminder_org_todo.rb
+++ beeminder/beeminder_org_todo.rb
@@ -24,7 +24,7 @@ goals = {
          :grammar       => ["languages/japanese.org",
                             "languages/french.org",
                             "languages/latin.org"],
-         :studienarbeit => "job/hiwi.org",
+         # :studienarbeit => "job/hiwi.org",
          :steno         => "steno/steno.org",
          :music         => "music/music.org",
         }
